564166250882f5d7a0032ae6c9fa91f6da12ac1b
Fernando <backovertime@gmail.com>
2017-03-10 01:35:27 +0100

    Variables añadidas

--- JavaMaster/src/variables/Vars.java
+++ JavaMaster/src/variables/Vars.java
@@ -1,5 +1,15 @@
 package variables;
 
 public class Vars {
-
+	
+	
+	public static void main(String[] args) {
+		//algunos tipos de variables
+		int num1;
+		float num2;
+		double num3;
+		String nombre;
+		boolean luz;
+		char letra;
+	}
 }
